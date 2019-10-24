95ffb9e46b28b77d0e93788fd63aed02733ca270
usama3 <baghdady.usama@gmail.com>
2019-10-24 21:26:55 +0300

    project

---
+++ TelerikWinFormsApp3/HashTable.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TelerikWinFormsApp3
+{
+    partial class HashTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn3 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            this.radTextBox1 = new Telerik.WinControls.UI.RadTextBox();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radButton3 = new Telerik.WinControls.UI.RadButton();
+            this.visualStudio2012DarkTheme1 = new Telerik.WinControls.Themes.VisualStudio2012DarkTheme();
+            this.radGridView1 = new Telerik.WinControls.UI.RadGridView();
+            this.radButton4 = new Telerik.WinControls.UI.RadButton();
+            this.radButton5 = new Telerik.WinControls.UI.RadButton();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radTextBox1
+            // 
+            this.radTextBox1.Location = new System.Drawing.Point(17, 46);
+            this.radTextBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.radTextBox1.Name = "radTextBox1";
+            this.radTextBox1.Size = new System.Drawing.Size(91, 21);
+            this.radTextBox1.TabIndex = 1;
+            this.radTextBox1.ThemeName = "VisualStudio2012Dark";
+            // 
+            // radButton2
+            // 
+            this.radButton2.Location = new System.Drawing.Point(17, 91);
+            this.radButton2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(91, 24);
+            this.radButton2.TabIndex = 2;
+            this.radButton2.Text = "Add";
+            this.radButton2.ThemeName = "VisualStudio2012Dark";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(17, 132);
+            this.radButton1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(91, 24);
+            this.radButton1.TabIndex = 3;
+            this.radButton1.Text = "Search";
+            this.radButton1.ThemeName = "VisualStudio2012Dark";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // radButton3
+            // 
+            this.radButton3.Location = new System.Drawing.Point(17, 177);
+            this.radButton3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.radButton3.Name = "radButton3";
+            this.radButton3.Size = new System.Drawing.Size(91, 24);
+            this.radButton3.TabIndex = 4;
+            this.radButton3.Text = "Delete";
+            this.radButton3.ThemeName = "VisualStudio2012Dark";
+            this.radButton3.Click += new System.EventHandler(this.radButton3_Click);
+            // 
+            // radGridView1
+            // 
+            this.radGridView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.radGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.radGridView1.EnableHotTracking = false;
+            this.radGridView1.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.radGridView1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radGridView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.radGridView1.Location = new System.Drawing.Point(129, 21);
+            // 
+            // radGridView1
+            // 
+            gridViewTextBoxColumn3.EnableExpressionEditor = false;
+            gridViewTextBoxColumn3.HeaderText = "Index";
+            gridViewTextBoxColumn3.MinWidth = 0;
+            gridViewTextBoxColumn3.Name = "column1";
+            gridViewTextBoxColumn3.ReadOnly = true;
+            gridViewTextBoxColumn3.RowSpan = 0;
+            gridViewTextBoxColumn3.Width = 60;
+            this.radGridView1.MasterTemplate.Columns.AddRange(new Telerik.WinControls.UI.GridViewDataColumn[] {
+            gridViewTextBoxColumn3});
+            this.radGridView1.Name = "radGridView1";
+            this.radGridView1.ReadOnly = true;
+            this.radGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.radGridView1.ShowGroupPanel = false;
+            this.radGridView1.Size = new System.Drawing.Size(537, 289);
+            this.radGridView1.TabIndex = 6;
+            this.radGridView1.Text = "radGridView1";
+            this.radGridView1.ThemeName = "VisualStudio2012Dark";
+            this.radGridView1.Click += new System.EventHandler(this.radGridView1_Click);
+            // 
+            // radButton4
+            // 
+            this.radButton4.Location = new System.Drawing.Point(17, 223);
+            this.radButton4.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.radButton4.Name = "radButton4";
+            this.radButton4.Size = new System.Drawing.Size(91, 24);
+            this.radButton4.TabIndex = 7;
+            this.radButton4.Text = "Randomize";
+            this.radButton4.ThemeName = "VisualStudio2012Dark";
+            this.radButton4.Click += new System.EventHandler(this.radButton4_Click);
+            // 
+            // radButton5
+            // 
+            this.radButton5.Location = new System.Drawing.Point(17, 271);
+            this.radButton5.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.radButton5.Name = "radButton5";
+            this.radButton5.Size = new System.Drawing.Size(91, 24);
+            this.radButton5.TabIndex = 8;
+            this.radButton5.Text = "Clear";
+            this.radButton5.ThemeName = "VisualStudio2012Dark";
+            this.radButton5.Click += new System.EventHandler(this.radButton5_Click);
+            // 
+            // HashTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 338);
+            this.Controls.Add(this.radButton5);
+            this.Controls.Add(this.radButton4);
+            this.Controls.Add(this.radGridView1);
+            this.Controls.Add(this.radButton3);
+            this.Controls.Add(this.radButton1);
+            this.Controls.Add(this.radButton2);
+            this.Controls.Add(this.radTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximizeBox = false;
+            this.Name = "HashTable";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HashTable";
+            this.ThemeName = "VisualStudio2012Dark";
+            this.Load += new System.EventHandler(this.HashTable_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadTextBox radTextBox1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadButton radButton3;
+        private Telerik.WinControls.Themes.VisualStudio2012DarkTheme visualStudio2012DarkTheme1;
+        private Telerik.WinControls.UI.RadGridView radGridView1;
+        private Telerik.WinControls.UI.RadButton radButton4;
+        private Telerik.WinControls.UI.RadButton radButton5;
+    }
+}
---
+++ TelerikWinFormsApp3/HashTable.cs
@@ -0,0 +1,204 @@
+﻿//MIT License
+
+//Copyright (c) 2019 Usama Albaghdady
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.Pivot.Core;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+
+namespace TelerikWinFormsApp3
+{
+    public partial class HashTable : Telerik.WinControls.UI.RadForm
+    {
+        int i = 1;
+        List<List<string>> l = new List<List<string>>(999);
+
+        void init()
+        {
+            for (int i = 0; i < 999; i++)
+            {
+                radGridView1.Rows.Add(i);
+                List<string> sublist = new List<string>();
+                l.Add(sublist);
+            }
+        }
+        public HashTable()
+        {
+            InitializeComponent();
+            init();
+
+        }
+
+        private void HashTable_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int HashFunction(string s)
+        {
+            long x = Convert.ToInt64(s);
+            long x1 = x / 10000;
+            long x2 = x % 10000;
+            int sum = Convert.ToInt16(x1 + x2);
+            return sum%999;
+        }
+
+        private void radButton2_Click(object sender, EventArgs e)
+        {
+            if (radTextBox1.Text == "" )
+                MessageBox.Show("insert a value");
+            else
+            {
+                int j = 1;
+
+                l[HashFunction(radTextBox1.Text)].Add(Convert.ToString(radTextBox1.Text));
+
+                if (radGridView1.Rows[HashFunction(radTextBox1.Text)].Cells.Count <= l[HashFunction(radTextBox1.Text)].Count)
+                    radGridView1.Columns.Add(i++.ToString());
+
+                while (radGridView1.Rows[HashFunction(radTextBox1.Text)].Cells[j].Value != null)
+                    j++;
+
+
+                radGridView1.Rows[HashFunction(radTextBox1.Text)].Cells[j].Value = Convert.ToString(radTextBox1.Text);
+                radGridView1.Columns[j].Width = 80;
+                MessageBox.Show("Added " + radTextBox1.Text + " at index " + HashFunction(radTextBox1.Text).ToString());
+                radTextBox1.Clear();
+            }
+        }
+
+        private void radGridView1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        bool search()
+        {
+            foreach (var v in l[HashFunction(radTextBox1.Text)])
+            {
+                if (v == radTextBox1.Text)
+                {
+                    return true;              
+                }
+            }
+            return false;              
+
+        }
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            if (radTextBox1.Text == "")
+                MessageBox.Show("insert a value");
+            else
+            {
+                if (search())
+                    MessageBox.Show("Found at index " + HashFunction(radTextBox1.Text));
+                else
+                    MessageBox.Show("Not Found");
+
+                radTextBox1.Clear();
+            }
+        }
+
+        private void radButton3_Click(object sender, EventArgs e)
+        {
+            if (radTextBox1.Text == "")
+                MessageBox.Show("insert a value");
+            else
+            {
+                if (search())
+                {
+                    int j = 1, p = -1;
+                    MessageBox.Show("Removed From index " + HashFunction(radTextBox1.Text).ToString());
+                    l[HashFunction(radTextBox1.Text)].Remove(radTextBox1.Text);
+
+                    radGridView1.Rows.Clear();
+                    init();
+                    foreach (var list in l)
+                    {
+                        int k = 1;
+                        p++;
+                        foreach (var str in list)
+                        {
+                            radGridView1.Rows[p].Cells[k++].Value = str;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Not Found");
+                }
+                radTextBox1.Clear();
+            }
+        }
+
+        private void radButton4_Click(object sender, EventArgs e)
+        {
+            radGridView1.Rows.Clear();
+            init();
+            int p = -1, c = 1;
+
+            Random r =new Random();
+            Random z = new Random();
+            for (int i = 0; i < 999; i++)
+            {
+                 int x  =  r.Next(0, 21);
+                for (int j = 0; j < x; j++)
+                {
+                   string res= z.Next(0, 99999999).ToString();
+                    l[HashFunction(res)].Add(res);
+                }
+            }
+
+            foreach (var list in l)
+            {
+                int k = 1;
+                p++;
+                foreach (var str in list)
+                {
+                    if (radGridView1.Rows[p].Cells.Count <= list.Count)
+                    {
+                        radGridView1.Columns.Add(c.ToString());
+                        radGridView1.Columns[c++].Width = 90;
+                    }
+                    radGridView1.Rows[p].Cells[k++].Value = str;
+                }
+            }
+            radButton4.Enabled = false;
+        }
+
+        private void radButton5_Click(object sender, EventArgs e)
+        {
+            radGridView1.Rows.Clear();
+            radGridView1.Columns.Clear();
+            radGridView1.Columns.Add("Index");
+            init();
+            radButton4.Enabled = true;
+
+        }
+    }
+}
